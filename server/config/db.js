66ffb864dc5c54ef3b74d766cb5d939b35d21c3d
Tee <cickpoo0121@gmail.com>
2022-06-10 14:45:29 +0700

    set up

---
+++ server/config/db.js
@@ -0,0 +1,12 @@
+require("dotenv").config();
+const mysql = require("mysql2");
+
+let connection = mysql.createConnection({
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASS,
+  database: process.env.DB_DATABASE,
+  port: process.env.DB_PORT,
+});
+
+module.exports = connection;
